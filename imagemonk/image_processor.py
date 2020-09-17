b47c9fbf58bc12d18b2b990c90bc5030f06a18ba
ejbosia <ejbosia@gmail.com>
2020-09-16 23:13:05 -0400

    Adding some functions
    Adding functions to crop, rotate, and flip the image

---
+++ imagemonk/image_processor.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+
+# crop an image
+def crop(image, x, y, width, height):
+    
+    # TODO add some error checking
+    
+    return image[y:y+height, x:x+width]
+
+
+# rotate an image 90 CW
+def rotate_CW(image):
+    return cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+
+
+# rotate an image 90 CCW
+def rotate_CCW(image):
+    return cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+# rotate an image 180
+def rotate_CCW(image):
+    return cv2.rotate(image, cv2.ROTATE_180)
+
+
+# flip an image vertically
+def flip_vertical(image):
+    return cv2.flip(image, 0)
+
+
+# flip an image horizontally
+def flip_horizontal(image):
+    return cv2.flip(image, 1)
+
+
+# debugging
+def main():
+    image = np.zeros((5,5))
+
+    image[0,0] = 255
+    image[1,1] = 255
+    image[0,2] = 255
+
+    cv2.imwrite("test.png", image)
+    cv2.imwrite("flip.png", flip(image))
+
+
+
+
+if __name__ == main():
+    main()
