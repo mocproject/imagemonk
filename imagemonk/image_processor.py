6e8a3d7f2ba4718a0f212588873f34ef0a7cb8ed
ejbosia <ejbosia@gmail.com>
2020-09-17 21:55:23 -0400

    Adding the resize function
    The resize function has error checking for right now an arbitrary size of image. It also handles tuple size or scaling factor input

--- imagemonk/image_processor.py
+++ imagemonk/image_processor.py
@@ -3,6 +3,15 @@ Module for image processing functions.
 '''
 import cv2
 
+# set the bounds of the image
+bounds = {
+    "MIN_width": 50,
+    "MIN_height": 50,
+    "MAX_width": 3000,
+    "MAX_height": 3000
+}
+
+
 # crop an image
 def crop(image, x, y, width, height):
     # TODO add some error checking
@@ -32,3 +41,41 @@ def flip_vertical(image):
 # flip an image horizontally
 def flip_horizontal(image):
     return cv2.flip(image, 1)
+
+
+def check_width(width):
+    if bounds["MIN_width"] > width or width > bounds["MAX_width"]:
+        raise ValueError("DIMENSION ERROR: WIDTH: " + str(width))
+    
+def check_height(height):
+    if bounds["MIN_height"] > height or height > bounds["MAX_height"]:
+        raise ValueError("DIMENSION ERROR: HEIGHT: " + str(height))
+    
+
+'''
+Resize image
+The image can either be resized using the input dimensions directly, or using the scaling factors. One or the other must be used. If both are supplied to the function, only the dimensions tuple will be used.
+'''
+def resize(image, dimensions=None, fx=0, fy=0):
+
+    # check if the parameters are inputted correctly
+    if dimensions is None and fx == 0 and fy == 0:
+        raise AttributeError("NO PARAMETERS INPUTTED")
+    
+    # if the dimensions tuple is inputted, do not use the scaling factors
+    if dimensions is not None:
+    
+        check_width(dimensions[0])
+        check_height(dimensions[1])
+
+        return cv2.resize(image, dimensions, interpolation = cv2.INTER_CUBIC)
+
+    # if the dimensions tuple is not inputted, use the scaling factors
+    else:
+
+        check_width(fx * image.shape[1])
+        check_height(fy * image.shape[0])
+
+        return cv2.resize(image, None, fx=fx, fy=fy, interpolation = cv2.INTER_CUBIC)
+
+
